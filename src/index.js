e155767ab53152d57c36333a733386d56693aaf6
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-07-25 22:17:57 +0300

    goit-js-hw-10_v_03

--- src/index.js
+++ src/index.js
@@ -20,47 +20,61 @@ fetchBreeds().then(cats => {
     for (let i = 0; i < cats.length; i += 1) {
         const option = document.createElement('option');
         option.value = cats[i].id;
-        option.text = cats[i].name;
+        option.textContent = cats[i].name;
         refs.select.append(option);
+        
     }
-    
+   
 // console.log(cats[i].name)
 
 }).catch(err => console.log(err));
 
+
+
+
 refs.select.addEventListener('change', onChange);
 
 function onChange() {
+    
     let id = refs.select.value;
    
     fetchCatByBreed(id).then(cat => {
         
+        console.log(cat)
         let img = cat[0].url;
-        // console.log(img)
-        
-        fetchBreeds().then(cats => {
-            const cat = cats.find(cat => cat.id === id);
-            // console.log(cat);
-            // console.log(cat.name);
-            // console.log(cat.description);
-            // console.log(cat.temperament);
-            // console.log(refs.catInfo);
-            // console.log(createMarkup(cat));
-            // const { name, description, temperament } = cat;
-            return cat;
-    }).then((data) => (refs.catInfo.innerHTML = createMarkup(data)))
-        
+        console.log(img)
+        fetchBreeds()
+            .then(cats => {
+                const cat = cats.find(cat => cat.id === id); 
+                return cat;  
+            })
+            .then(data => (refs.catInfo.innerHTML = createMarkup(img, data)))
+        // (refs.catInfo.innerHTML = createMarkup(img, cat))
+        // fetchBreeds().then(cats => {
+        //     const cat = cats.find(cat => cat.id === id);
+        //     // console.log(cat);
+        //     // console.log(cat.name);
+        //     // console.log(cat.description);
+        //     // console.log(cat.temperament);
+        //     // console.log(refs.catInfo);
+        //     // console.log(createMarkup(cat));
+        //     // const { name, description, temperament } = cat;
+        //     return cat;
+        //     // }).then((data) => (refs.catInfo.innerHTML = createMarkup(data)))
+        // }).then((data) => (refs.catInfo.innerHTML = createMarkup(data))) 
     }).catch(err => console.log(err));
     
 };
 
-function createMarkup(obj) {
-    // const { name, description, temperament } = obj;
-    return markup = (({ name, description, temperament }) =>
-        `<h2>${name}</h2>
-         <p>${description}</p>
-         <p>Temperament:${temperament}</p>`).join('')
+function createMarkup(img, data) {
     
-    // return markup;
-} 
+    const { name, description, temperament } = data;
+    const markup = (img, ({ name, description, temperament }) => 
+        `<img src="${img}" alt="${name}">
+        <h2>${name}</h2>
+         <p>${description}</p>
+         <p>Temperament:${temperament}</p>`);
+    
+    return markup;
+}; 
     
