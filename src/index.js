a85b650e50c24b764478939dc7d81c2d76b905e7
Marjan Bat'kivs'kiy <igorovych@gmail.com>
2023-07-30 22:35:33 +0300

    goit-js-hw-10_v_04

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import axios from "axios";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
 
 axios.defaults.headers.common["x-api-key"] = "live_SrdRs6FoQE4ieaTlJFUjw3DEORVGo9Zy1ZwdJFuuaIWVL7YLLnhj1hZ5ZUzJCOwa";
 
+
+
 const refs = {
     select: document.querySelector('.breed-select'),
     catInfo: document.querySelector('.cat-info'),
+    eror: document.querySelector('.error')
     
 };
+
+refs.eror.classList.add('visually-hidden');
+// refs.eror.style.opacity = "0";
+
+
 
 fetchBreeds().then(cats => {
     
@@ -19,7 +28,7 @@ fetchBreeds().then(cats => {
         
     }
 
-}).catch(err => console.log(err));
+}).catch(eror => Notify.failure(refs.eror.textContent));
 
 refs.select.addEventListener('change', onChange);
 
@@ -39,7 +48,7 @@ function onChange() {
             })
             .then(data => (refs.catInfo.innerHTML = createMarkup(img, data)))
          
-    }).catch(err => console.log(err));
+    }).catch(err => Notify.failure(refs.eror.textContent));
     
 };
 
@@ -47,17 +56,17 @@ function createMarkup(img, data) {
     
     const { name, description, temperament } = data;
     const markup =  
-        `<img class="cat-img" src="${img}" alt="${name}" width="250">
+        `<img class="cat-img" src="${img}" alt="${name}" width="250" height="200">
         <div class="cat-container">
             <h2 class="cat-name" >${name}</h2>
             <p class="cat-description" >${description}</p>
-            <p class="cat-temperament">
-                <span class="cat-temperament-title" > Temperament: </span>
-                ${temperament}</p>
+            <h4>Temperament:</h4>
+            <p>${temperament}</p>    
         </div>`;
     
     refs.catInfo.style.display = "flex";
     refs.catInfo.style.gap = "20px";
+    
     
     return markup;
 }; 
